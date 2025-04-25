d901530f90c31b1c57acbc44b9c9e1a47145cbe5
AnteroJunior <anterojunior1530@gmail.com>
2025-04-25 06:21:53 -0300

    feat: schedule module

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Schedule(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    matricula = models.CharField(max_length=20)
+    course = models.CharField(max_length=100)
+    period = models.CharField(max_length=20)
+    reason = models.TextField()
+    availability = models.CharField(max_length=100)
+    psychologist = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
