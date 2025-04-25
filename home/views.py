0dc4b20fcaa65de126002dcc69f522e1a1250965
AnteroJunior <anterojunior1530@gmail.com>
2025-04-25 06:21:17 -0300

    feat: home module

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+def create(request):
+    return render(request, 'home/register.html')
+
+#def login(request):
+ #   return render(request, 'oi')
